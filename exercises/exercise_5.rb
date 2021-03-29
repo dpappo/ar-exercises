a58923c9d554a0c05d90562f1cd79280f54739b8
Dan Pappo <dpappo@gmail.com>
2021-03-29 16:27:36 -0400

    exercise 5 answer

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Sum #{Store.sum(:annual_revenue)}"
+puts "Average #{Store.average(:annual_revenue)}"
+@over_millie = Store.where(["annual_revenue > ?", "1000000"])
+puts "#{@over_millie.length} stores with over 1,000,000 in revenue"
